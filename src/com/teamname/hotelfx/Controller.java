718063f54e513d8d0f5e8419cb01d468987dc65a
Alex <codefactory2222@hotmail.com>
2020-01-22 13:00:52 +0100

    minor visual updates

--- src/com/teamname/hotelfx/Controller.java
+++ src/com/teamname/hotelfx/Controller.java
@@ -541,6 +541,8 @@ public class Controller {
 
     public void initialize() throws SQLException, ParseException {
 
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
         /*add change listeners to tableViews*/
         guest_tableView.getSelectionModel().selectedIndexProperty().addListener(
                 new ListSelectChangeListener(guest_tableView)
